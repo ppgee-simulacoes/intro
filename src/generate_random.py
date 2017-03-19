c545581e1700554a4649f7565c4751d1c6ba801b
CalilQ <calil_queiroz@hotmail.com>
2017-03-19 19:23:19 -0300

    Implement uniform random distribution
    Uniform random distribution implemented in class GenerateRandom and tested in GenerateRandomTest.

--- src/generate_random.py
+++ src/generate_random.py
@@ -5,18 +5,22 @@ Created on Sun Mar 19 16:33:54 2017
 @author: Calil
 """
 
-#import numpy as np
+import numpy as np
 #import math
 
 class GenerateRandom(object):
     
-    def __init__(self):
-        self.__seed = 0
-        self.__num_samples = 0
+    def __init__(self, seed, num_samples):
+        self.__seed = seed
+        self.__num_samples = num_samples
+        np.random.seed(seed)
         
     def get_seed(self):
         return self.__seed
     
     def get_num_samples(self):
         return self.__num_samples
+    
+    def uniform(self):
+        return np.random.rand(1,self.get_num_samples)
         
