a6b875fa8dd0e84f9044b58abf315fca01892203
CalilQ <calil_queiroz@hotmail.com>
2017-03-19 15:49:55 -0300

    Created main script, from where execution starts.

---
+++ src/main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Main script.
+
+Created on Sun Mar 19 14:46:39 2017
+
+@author: Calil
+"""
+
+seed = 10
+numSamples = 100
+
+print("\n\n\nInitiating...")
+print("\nSeed = ", seed)
+print("\nNumber of samples = ", numSamples)
