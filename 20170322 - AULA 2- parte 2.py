0626ed93913cb447150505ab6306eccd1a493277
rafaelprata1 <rafaelprata@anatel.gov.br>
2017-03-22 21:51:02 -0300

    Task 2nd class (16-03) , part 2

---
+++ 20170322 - AULA 2- parte 2.py
@@ -0,0 +1,16 @@
+import numpy
+import matplotlib.pyplot as plt
+
+#calculate and plot PDF
+print ("Calculo e plot da PDF")
+0 < x < 10
+h = plt.hist(np.random.triangular(0, A*x, 10, 10000000), bins=200, normed=True)
+plt.show()
+
+#calculate and plot CDF
+print ("Calculo e plot da CDF")
+x1=A*x
+num_bins_plot_cdf_x1 = 20
+counts, bin_edges_x1 = np.histogram(x1, bins=num_bins_plot_cdf_x1, normed=True)
+cdf_x1 = np.cumsum(counts)
+pylab.plot(bin_edges_z1[1:], cdf_x1)
