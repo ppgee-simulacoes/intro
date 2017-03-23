0c910147c1abfedb856304275497a152a4364cc1
rafaelprata1 <rafaelprata@anatel.gov.br>
2017-03-23 08:39:38 -0300

    Update 20170322 - AULA 2- parte 2.py
    Code correction, inclusion of import Numpy as np and import pylab which were missing.

--- 20170322 - AULA 2- parte 2.py
+++ 20170322 - AULA 2- parte 2.py
@@ -1,9 +1,11 @@
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
+import pylab
 
 #calculate and plot PDF
 print ("Calculo e plot da PDF")
-0 < x < 10
+x=10
+A=1
 h = plt.hist(np.random.triangular(0, A*x, 10, 10000000), bins=200, normed=True)
 plt.show()
 
@@ -13,4 +15,4 @@ x1=A*x
 num_bins_plot_cdf_x1 = 20
 counts, bin_edges_x1 = np.histogram(x1, bins=num_bins_plot_cdf_x1, normed=True)
 cdf_x1 = np.cumsum(counts)
-pylab.plot(bin_edges_z1[1:], cdf_x1)
+pylab.plot(bin_edges_x1[1:], cdf_x1)
