3e4d8641679fe92ab01c89348de590d9a0775294
CalilQ <calil_queiroz@hotmail.com>
2017-03-26 12:05:15 -0300

    Document Source class
    Add comments and docstring to Source class' methods.

--- Exercise 2/src/source.py
+++ Exercise 2/src/source.py
@@ -11,25 +11,35 @@ import numpy as np
 
 class Source(object):
     def __init__(self,n_bits,seed):
-           
-        self.__n_bits = n_bits
-        self.__seed = seed
+        self.__n_bits = n_bits                 # number of bits per packet 
+        self.__seed = seed                     # random number generator seed
+        self.__last_pck = np.zeros([1,n_bits]) # last transmitted packet
         self.__rnd_state = np.random.RandomState(seed)
-        self.__last_pck = np.zeros([1,n_bits])
     
     def get_n_bits(self):
+        """Returns the number of bits per packet."""
         return self.__n_bits
     
     def get_seed(self):
+        """Returnt the random number generator seed."""
         return self.__seed
     
     def get_last_pck(self):
+        """Returns a reference to the last transmitted packet."""
         return self.__last_pck
     
     def get_rnd_state(self):
+        """Returns the random state object."""
         return self.__rnd_state
     
     def generate_packet(self):
+        """
+        Returns a new, random generated, 0s and 1s packet.
+        Also updates the last transmitted packet to new one.
+        
+        Returns:
+            pck -- generated packet
+        """
         pck = self.get_rnd_state().randint(2,\
                                             size=self.get_n_bits())
         # Last packet is a copy of sent packet, for further comparison
@@ -38,6 +48,16 @@ class Source(object):
         return pck
     
     def calculate_error(self,pck):
+        """
+        Calculates bit and packet error.
+        
+        Keyword arguments:
+            pck --  received packet, to be compared with last packet
+            
+        Returns:
+            n_errors -- number of bit errors in received packet
+            pck_error -- boolean, true if packet contains errors
+        """
         n_errors = np.sum(pck != self.get_last_pck())
         pck_error = (n_errors != 0)
         
