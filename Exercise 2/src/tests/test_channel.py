349785238ccd3e798c34e50998c505fc391de760
CalilQ <calil_queiroz@hotmail.com>
2017-03-27 08:43:38 -0300

    Implement Channel class tests
    Add tests for ideal channel, in test_channel.py

--- Exercise 2/src/tests/test_channel.py
+++ Exercise 2/src/tests/test_channel.py
@@ -11,7 +11,36 @@ import unittest
 import numpy as np
 
 from . import Channel
+from .support import ChannelModel
 
 class ChannelTest(unittest.TestCase):
-    pass
+    def setUp(self):
+        seed = 10
+        self.n_bits = 1000
+        self.channel1 = Channel(ChannelModel.IDEAL,seed)
+        self.channel2 = Channel(ChannelModel.CONSTANT,seed)
+        self.channel3 = Channel(ChannelModel.MARKOV,seed)
         
+    def test_get_model(self):
+        self.assertEqual(ChannelModel.IDEAL,\
+                         self.channel1.get_model())
+        self.assertEqual(ChannelModel.CONSTANT,\
+                         self.channel2.get_model())
+        self.assertEqual(ChannelModel.MARKOV,\
+                         self.channel3.get_model())
+        
+    def test_get_seed(self):
+        self.assertEqual(10,self.channel1.get_seed())
+        
+    def test_fade(self):
+        """
+        TODO Implement unit test for constant and Markov channels
+        """
+        pck_Tx = np.zeros([1,self.n_bits])
+        
+        # Ideal channel should cause no bit errors
+        pck_Rx = self.channel1.fade(pck_Tx)
+        self.assertEqual(0,np.sum(pck_Rx))        
+        
+if __name__ == '__main__':
+    unittest.main()
