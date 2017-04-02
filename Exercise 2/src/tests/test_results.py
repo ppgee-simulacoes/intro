71964339998f5b424b8ba5b5b421e854acba0ffd
CalilQ <calil_queiroz@hotmail.com>
2017-04-02 11:02:51 -0300

    Implement Results class
    Results class saves and plots simulation results.

--- Exercise 2/src/tests/test_results.py
+++ Exercise 2/src/tests/test_results.py
@@ -16,50 +16,62 @@ class ResultsTest(unittest.TestCase):
     
     def setUp(self):
         param = Parameters(1)
-        self.res = Results(param)
+        figs_dir = "test_figs/"
+        self.res = Results(param, figs_dir)
+        
+    def test_get_param(self):
+        par = self.res.get_param()
+        self.assertEqual(20,len(par.p))
+        
+    def test_get_figs_dir(self):
+        f_dir = self.res.get_figs_dir()
+        self.assertEqual("test_figs/",f_dir)
         
     def test_get_per_list(self):
-        per = self.per.get_per_list()
+        per = self.res.get_per_list()
         self.assertEqual(0,len(per))
         
     def test_get_per_conf(self):
-        per_conf = self.per.get_per_conf()
+        per_conf = self.res.get_per_conf()
         self.assertEqual(0,len(per_conf))
     
     def test_get_thrpt_list(self):
-        thrpt = self.per.get_thrpt_list()
+        thrpt = self.res.get_thrpt_list()
         self.assertEqual(0,len(thrpt))
     
     def test_get_thrpt_conf(self):
-        thrpt_conf = self.per.get_thrpt_conf()
+        thrpt_conf = self.res.get_thrpt_conf()
         self.assertEqual(0,len(thrpt_conf))
         
     def test_store_res_plot(self):
-        per_tpl = (1.0e-05, 1.0e-0.6)
-        thrpt_tpl = (2.0e-05, 2.0e-0.6)
+        per_tpl = (1.0e-05, 1.0e-06)
+        thrpt_tpl = (50, 0.5)
         
         # Add 100 PER and Throughput results
-        for k in xrange(0,100):
+        len_p = len(self.res.get_param().p)
+        for k in range(0,len_p):
             self.res.store_res(per_tpl,thrpt_tpl)
             
         # Check PER length
-        per = self.per.get_per_list()
-        self.assertEqual(100,len(per))
+        per = self.res.get_per_list()
+        self.assertEqual(20,len(per))
         
         # Check PER confidence length
-        per_conf = self.per.get_per_conf()
-        self.assertEqual(100,len(per_conf))
+        per_conf = self.res.get_per_conf()
+        self.assertEqual(20,len(per_conf))
         
         # Check thrpt length
-        thrpt = self.per.get_thrpt_list()
-        self.assertEqual(100,len(thrpt))
+        thrpt = self.res.get_thrpt_list()
+        self.assertEqual(20,len(thrpt))
         
         # Check thrpt_conf lenth
-        thrpt_conf = self.per.get_thrpt_conf()
-        self.assertEqual(100,len(thrpt_conf))
+        thrpt_conf = self.res.get_thrpt_conf()
+        self.assertEqual(20,len(thrpt_conf))
         
         # Plot
-        self.res.plot()
+        theo_per = 1.0e-05*np.ones([len_p,])
+        theo_thrpt = 50*np.ones([len_p,])
+        self.res.plot(theo_per,theo_thrpt)
 
 if __name__ == '__main__':
     unittest.main()
